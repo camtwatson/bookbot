912fb7201d52237010807d9de07123c86e28c8f7
Cam <cam.t.watson@gmail.com>
2025-07-14 13:33:35 -0400

    refactor

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
+from stats import get_words
 def get_book_test(book):
     with open(book) as f:
         content = f.read()
 #    print(content)
     get_words(content)
-
-
-def get_words(content):
-    words = content.split()
-    maybe = len(words)
-    print(f"{maybe} words found in the document.")
 
 def main():
     book = "books/frankenstein.txt"
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_words(content):
+    words = content.split()
+    maybe = len(words)
+    print(f"{maybe} words found in the document.")
