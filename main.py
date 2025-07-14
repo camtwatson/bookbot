297d5d3242f1f43c56b578bec969a6f3a490887d
Cam <cam.t.watson@gmail.com>
2025-07-14 13:49:44 -0400

    count symbols

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from stats import get_words
+from stats import get_characters
 def get_book_test(book):
     with open(book) as f:
         content = f.read()
 #    print(content)
     get_words(content)
+    get_characters(content)
 
 def main():
     book = "books/frankenstein.txt"
     get_book_test(book)
+
 
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,13 @@ def get_words(content):
     words = content.split()
     maybe = len(words)
     print(f"{maybe} words found in the document.")
+
+def get_characters(content):
+    how_many = {}
+    lowers = content.lower()
+    for l in lowers:
+        if l not in how_many:
+            how_many[l] = 1
+        else:
+            how_many[l] += 1
+    print(how_many)
