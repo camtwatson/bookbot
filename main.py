7a6b2bdd5b0b48baf8959ecc01be97ae9c53601d
Cam <cam.t.watson@gmail.com>
2025-07-15 17:07:06 -0400

    get the right output

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from stats import get_words
 from stats import get_characters
+from stats import sort_characters
+
+
 def get_book_test(book):
     with open(book) as f:
         content = f.read()
 #    print(content)
     get_words(content)
     get_characters(content)
+    
 
 def main():
     book = "books/frankenstein.txt"
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     get_book_test(book)
 
 
 
-main()
+main() 
--- stats.py
+++ stats.py
@@ -1,7 +1,8 @@
 def get_words(content):
     words = content.split()
     maybe = len(words)
-    print(f"{maybe} words found in the document.")
+    print(f"Found {maybe} total words")
+    print("--------- Character Count -------")
 
 def get_characters(content):
     how_many = {}
@@ -11,4 +12,25 @@ def get_characters(content):
             how_many[l] = 1
         else:
             how_many[l] += 1
-    print(how_many)
+    sort_characters(how_many)
+
+def sort_characters(how_many):
+    final = []
+    for t in how_many.items():
+        dic = {}
+        dic["char"] = t[0]
+        dic["num"] = t[1]
+        final.append(dic)
+    final.sort(key=sort_on, reverse=True)
+
+    for f in final:
+        t = f["char"]
+        if t.isalpha():
+            print(f"{t}: {f['num']}")
+    print("============= END ===============")
+
+def sort_on(item):
+    return item["num"]
+
+
+
