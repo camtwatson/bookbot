779fc224c0cc22a92a57bf8efe8f52f8a0a69601
Cam <cam.t.watson@gmail.com>
2025-07-16 10:43:46 -0400

    add sys args

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from stats import get_words
 from stats import get_characters
 from stats import sort_characters
+import sys
 
+def check_args():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    print(sys.argv[1])
+    return sys.argv[1]
 
 def get_book_test(book):
     with open(book) as f:
@@ -9,10 +16,11 @@ def get_book_test(book):
 #    print(content)
     get_words(content)
     get_characters(content)
-    
+
 
 def main():
-    book = "books/frankenstein.txt"
+    check_args()
+    book = sys.argv[1]
     print("============ BOOKBOT ============")
     print("Analyzing book found at books/frankenstein.txt...")
     print("----------- Word Count ----------")
